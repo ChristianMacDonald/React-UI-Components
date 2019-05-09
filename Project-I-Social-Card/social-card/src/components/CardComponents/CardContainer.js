fb15bdfdcf175ab83516a54f98aa13c98e8c9fab
ChristianMacDonald <chrismacd412@gmail.com>
2019-05-08 22:11:53 -0400

    Update CardContainer.js

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
+import CardBanner from './CardBanner.js';
+import CardContent from './CardContent.js';
 import './Card.css';
+
+class CardContainer extends React.Component {
+    render() {
+        return (
+            <div className="card-container">
+                <CardBanner />
+                <CardContent />
+            </div>
+        );
+    }
+}
