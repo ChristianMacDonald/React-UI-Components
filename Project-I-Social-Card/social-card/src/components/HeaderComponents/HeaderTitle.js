cfb91bc1ba3be623db93ef8ec52e6a93b077920d
ChristianMacDonald <chrismacd412@gmail.com>
2019-05-08 21:21:24 -0400

    Update HeaderTitle.js

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+class HeaderTitle extends React.Component {
+    render() {
+        return (
+            <div>
+                <span>LambdaSchool</span>
+                <span>{"@"}LambdaSchool</span>
+                <span>26 jan</span>
+            </div>
+        );
+    }
+}
