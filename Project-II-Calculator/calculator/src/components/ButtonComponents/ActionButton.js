e07b3e1f2de6332be3776e39754208d851f54eb8
ChristianMacDonald <chrismacd412@gmail.com>
2019-05-11 13:59:26 -0400

    Update ActionButton.js

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+class ActionButton extends React.Component {
+    render() {
+        return (
+            <button className="action-button">{this.props.label}</button>
+        );
+    }
+}
+
+export default ActionButton;
