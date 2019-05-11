039073e5f72e0836da83e989fc6b5ebf6fecbc6d
ChristianMacDonald <chrismacd412@gmail.com>
2019-05-11 14:01:03 -0400

    Update NumberButton.js

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+class NumberButton extends React.Component {
+    render() {
+        return (
+            <button className="number-button">{this.props.label}</button>
+        );
+    }
+}
